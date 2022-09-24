e03759aa6eda3e02bc2a74036258a8c0c54f4083
huanghf <1399859803@qq.com>
2022-09-24 11:14:33 +0800

    修改网站图标

--- src/layouts/hooks/useUserInfo.ts
+++ src/layouts/hooks/useUserInfo.ts
@@ -1,6 +1,6 @@
 import { reactive, Ref } from 'vue'
-import localCache from '@/utils/cache'
 import { useUserStore } from '@/store/user'
+
 export default function useUserInfo(isShowPic: Ref) {
   const user = reactive({
     id: '',
